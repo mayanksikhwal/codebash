6856cf557d6d2d341c7e8b0419289cb8cdc6acc3
Mayank Sikhwal <sikhwalmayank537@gmail.com>
2023-06-14 09:37:14 +0530

    Complete the project

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -15,7 +15,7 @@ const AuthModal: React.FC<AuthModalProps> = () => {
 		<>
 			<div
 				className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60'
-				//onClick={closeModal}
+				onClick={closeModal}
 			></div>
 			<div className='w-full sm:w-[450px]  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  flex justify-center items-center'>
 				<div className='relative w-full h-full mx-auto flex items-center justify-center'>
@@ -24,7 +24,7 @@ const AuthModal: React.FC<AuthModalProps> = () => {
 							<button
 								type='button'
 								className='bg-transparent  rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white text-white'
-								//onClick={closeModal}
+								onClick={closeModal}
 							>
 								<IoClose className='h-5 w-5' />
 							</button>
