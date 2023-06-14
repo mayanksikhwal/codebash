e1e18c54bf41c7190553ac312d9b7a2bbd296c7c
Mayank Sikhwal <sikhwalmayank537@gmail.com>
2023-06-14 09:01:51 +0530

    Complete the project

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	apiKey: "AIzaSyC7K3fdwFvnUHU8-2Q3Df0_XZwjTKdcyxo",
+	authDomain: "codebash-48a90.firebaseapp.com",
+	projectId: "codebash-48a90",
+	storageBucket: "codebash-48a90.appspot.com",
+	messagingSenderId: "1095624549121",
+	appId: "1:1095624549121:web:21a21aa8d4cc590981f100"
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
